3687675dc872f941f55ce1c7b15a514eae68bb4b
alperenacr <alprnacar@hotmail.com>
2023-02-07 18:40:52 +0300

    implemented the first  four method and constructor

--- Array.java
+++ Array.java
@@ -1,13 +1,33 @@
+import java.util.Random;
 public class Array {
-
-    // Constructor and method gonna be initialized...
-    // a
+    
+    //array instance variable
+    private int[] array;
+    //constructor
     public Array(int size){
+        // creating an int array that contains random integers in the range [0,100].
+        this.array = new int[size]; 
+        Random random = new Random();
+        for(int i = 0; i < size; i++){
+            this.array[i] = random.nextInt(0, 101);
+        }
     }
     
     // c
     public int findMin(){
-        int min = 0;
+        int min = 101;
+        //finding the smallest value in the int array
+        int temp;
+        for(int i = 0; i < this.array.length; i++){
+            for(int j = i + 1; j < this.array.length; j++){
+                temp = this.array[j];
+                
+                if(temp < min){
+                    min = temp;
+                }
+            }
+
+        }
         return min;
     }
 
@@ -20,12 +40,31 @@ public class Array {
     // d
     public int findAverage(){
         int average = 0;
+        int total = 0;
+        //calculation
+        for(int i = 0; i < this.array.length; i++){
+            total = total + this.array[i];
+        }
+        average = total / this.array.length;
         return average;
     }
 
     // d
     public String displayDifferences(){ 
-        String differences = "";
+        //crating a stringBuilder object
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('{');
+        //finding the differences
+        for(int i = 0; i < this.array.length; i++){
+            stringBuilder.append(this.array[i] - this.findAverage());
+            if(i != this.array.length - 1){
+                stringBuilder.append(',');
+                stringBuilder.append(' ');
+            } 
+        }
+        stringBuilder.append('}');
+        
+        String differences= stringBuilder.toString();
         return differences;
     }
 
